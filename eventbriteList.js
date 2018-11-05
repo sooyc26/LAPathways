10079cff14936e0e496faafd810149cf0a0854de
Soo Chu <sooyc26@gmail.com>
2018-11-05 15:25:26 -0800

    Update eventbriteList.js

--- eventbriteList.js
+++ eventbriteList.js
@@ -43,62 +43,9 @@ class EventBriteList extends React.Component {
             event.preventDefault();
         }
     }
+                {/* ...removed for brevity */}
 
-    filterList(event) {
-        let value = event.target.value;
-        let events = this.state.events, result = [];
-        let internalEventsArrFilter = this.state.internalEventsArrFilter, internalEventsArr = [];
-
-        internalEventsArr = internalEventsArrFilter.filter((event) => {
-            return event.name.text.toLowerCase().search(value) != -1
-            || event.description.text
-            && event.description.text.toLowerCase().search(value) != -1;
-        });
-
-        result = events.filter((event) => {
-            return event.name.text.toLowerCase().search(value) != -1
-                || event.description.text
-                && event.description.text.toLowerCase().search(value) != -1;
-        });
-        this.setState({ result, internalEventsArr });
-    }
-
-    onCardDetails(id,e) {
-        e.stopPropagation();
-        e.preventDefault();
-
-        this.props.history.push("/eventbrite/details/" + id)
-    }
-
-    render() {
-        const eventsList = this.state.result ? this.state.result.map((event) => {
-            return (
-
-                <div class="col-xl-4 col-lg-3 col-sm-4 col-6" onClick={e => { this.onCardDetails(event.id, e) }}>
-                    <div class="groups__item" onClick={(e) => { this.props.addEventBriteHistory(event) }}>
-                        <a href={event.url}>
-                            <div class="groups__img" style={{ marginTop: '200px' }}>
-                                <img className='card-img-overlay' src={event.logo ? event.logo.url : 'http://www.otczenacts.com/images/no-image.jpg'} style={{ height: '200px' }}></img>
-                            </div>
-                            <div class="groups__info">
-                                <a href={event.url} target='_blank'><strong>{event.name.text}</strong></a><br></br>
-                                <a href={event.url} target='_blank' className='card-link'>Click here for more information</a>
-                            </div>
-                        </a>
-                        <div class="actions">
-                            <div class="dropdown actions__item">
-                                <i class="zmdi zmdi-more-vert" data-toggle="dropdown"></i>
-
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <a class="dropdown-item" href="">Edit</a>
-                                    <a class="dropdown-item" href="" data-demo-action="delete-listing">Delete</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            );
-        }) : <LoadingSpinner loading={this.state.loading}></LoadingSpinner>;
+    render() {                {/* ...removed for brevity */}
 
         const internalEvents = this.state.internalEventsArr.map(event => {
             return (
@@ -142,7 +89,7 @@ class EventBriteList extends React.Component {
                         <i class="zmdi zmdi-search search__helper" data-sa-action="search-close"></i></div>
                 </form>
                 <div className='row groups'>
-                    {/* {eventsList} */}
+                {/* ...removed for brevity */}
                     {internalEvents}
                 </div>
             </div>
